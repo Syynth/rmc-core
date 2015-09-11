8135a58167101b5464413174bccd911906e64cc4
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-11 16:36:03 -0400

    Totally re-wrote createElement to make sense

--- src/createElement.js
+++ src/createElement.js
@@ -2,17 +2,16 @@ import { createElement } from 'react'
 
 import { isString as str, isFunction as fn } from './utils'
 
-function lookup(tagName) {
-  return tagName;
+function lookup(tagName, r) {
+  return r[tagName] || tagName;
 }
 
-function mapChildren(children) {
+function map(children, r) {
   return children.map(child => isString(child) ? child : render(child));
 }
 
-function render(element, props, ...children) {
-  let el = (str(element) || fn(element)) ? element : lookup(element.tagName);
-  return createElement(el, props, mapChildren(children));
+function render({ tagName, props, children }, r = {}) {
+  return createElement(lookup(tagName, r), props, ...map(children, r));
 }
 
 export { render as createElement };
