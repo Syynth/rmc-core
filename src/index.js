3da8c9e65b416ccd3fbf9eaefeb6e4d482341db1
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-10-10 01:52:07 -0400

    Index exports getRenderer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// export { renderData, RmcRenderer } from './renderer'
+export { getRenderer } from './renderer'
 export { ComponentRegistry } from './registry'
 export { createElement } from './createElement'
